c97922b3c34be151a09e18e26d78c68eb5035bda
nnadozieopara <nnadozieopara@yahoo.com>
2022-08-04 18:51:08 +0100

    added properties

--- CCSANoteApp.Domain/DTOs/FetchNoteDto.cs
+++ CCSANoteApp.Domain/DTOs/FetchNoteDto.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace CCSANoteApp.Domain.DTOs
 {
-    public class FetchNoteDto : NoteDto
+    public class FetchNoteDto 
     {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public GroupName GroupName { get; set; }
+        public string NoteCreatorUserName { get; set; }
+        public Guid NoteId { get; set; }
         public virtual DateTime CreatedDate { get; set; }
         public virtual DateTime UpdatedDate { get; set; }
     }
