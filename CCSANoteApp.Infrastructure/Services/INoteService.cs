8686d227527076f1fc3d8531817a3114d97c91fd
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-05 11:21:40 +0100

    Fixed cycle error

--- CCSANoteApp.Infrastructure/Services/INoteService.cs
+++ CCSANoteApp.Infrastructure/Services/INoteService.cs
@@ -12,7 +12,7 @@ namespace CCSANoteApp.Infrastructure
         void DeleteNote(List<Guid> notes);
         List<FetchNoteDto> FetchNote();
         List<Note> FetchNoteByUser(Guid id);
-        Note FetchNoteById(Guid id);
+        FetchNoteDto FetchNoteById(Guid id);
         List<Note> FetchUserNotesByGroup(Guid userId, GroupName groupName);
     }
 }
