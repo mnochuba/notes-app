0305aaccd35a30996ec514ffe392a210f8e0ad4f
gideon <edoghotugiddy@gmail.com>
2022-08-06 00:02:03 +0100

    Included user notes when fetching user for issue #11

--- CCSANoteApp.Domain/DTOs/FetchUserDto.cs
+++ CCSANoteApp.Domain/DTOs/FetchUserDto.cs
@@ -8,6 +8,7 @@ namespace CCSANoteApp.Domain.DTOs
 {
     public class FetchUserDto
     {
+        public Guid UserId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public List<FetchNoteDto> UserNotes { get; set; }
