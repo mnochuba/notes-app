a5a7797bf6d227cd872e0f777f0fb9177a1cee5e
Suzee27 <suzieoborokumo@gmail.com>
2022-08-07 21:01:27 +0100

    Added FetchNotesByGroup method to return a list of notes in a group

--- CCSANoteApp.DB/Repositories/NoteRepository.cs
+++ CCSANoteApp.DB/Repositories/NoteRepository.cs
@@ -19,5 +19,11 @@ namespace CCSANoteApp.DB.Repositories
             var notes = _session.Query<Note>().Where(x => x.NoteCreator.Id == userId);
             return notes.ToList();
         }
+
+        public List<Note> FetchNotesByGroup(GroupName groupName)
+        {
+            var notes = _session.Query<Note>().Where(x => x.GroupName == groupName);
+            return notes.ToList();
+        }
     }
 }
